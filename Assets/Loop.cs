9cf11702b0f09738d4c07a58c4629c375aec49cd
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-05-29 20:21:00 -0300

    tuberias se mueven

---
+++ Assets/Loop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop : MonoBehaviour
+{
+    private float velocidad = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        loop();
+    }
+    private void loop()
+    {
+        transform.Translate(Vector3.left * velocidad * Time.deltaTime);
+        if (transform.position.x<-10)
+        {
+            transform.Translate(Vector3.right * 20);
+        }
+    }
+}
