328d0d9712c75e79122bb663ac103e1a3897dadd
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-06-04 11:12:20 -0300

    timemanager

---
+++ Assets/TimeManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public Text txt_time;
+    public Text txt_timeFloored;
+    void Update()
+    {
+        float currentTime = Time.time;
+        if (currentTime % 2 == 0)
+        {
+            int i = 0;
+            Debug.Log(i);
+            i++;
+        }
+
+        txt_time.text = currentTime.ToString();
+
+        //txt_timeFloored.text = Mathf.Floor(currentTime).ToString();
+
+    }
+}
